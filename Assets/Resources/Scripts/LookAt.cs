7a09193d17e2162ec1b3b8af62e6063f25a3aa00
exe4que <exe4que@gmail.com>
2015-12-24 18:43:46 -0300

    Commit inicial

---
+++ Assets/Resources/Scripts/LookAt.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour
+{
+    public bool followMouse = false;
+    public GameObject targetObject;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (followMouse)
+        {
+            LookAtTarget(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+        else
+        {
+            LookAtTarget(targetObject.transform.position);
+        }
+    }
+
+    private void LookAtTarget(Vector3 _target)
+    {
+        Vector3 diff = _target - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
+}
